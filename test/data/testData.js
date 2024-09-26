1722f5e908c776f87b8b9438aabcfd149a37ffbb
qafellas <qafellas@users.noreply.github.com>
2024-09-26 05:23:27 +0000

    Automated deployment: Thu Sep 26 05:23:27 UTC 2024 358f88e56c8e73965d0c180b2b0a86933c7965ce

--- test/data/testData.js
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'
-
-export default  {
-   email: process.env.EMAIL,
-   password: process.env.PASSWORD,
-   url: process.env.URL,
-   invalidEmail: 'wert@gmail.com',
-   invalidPassword: 'ededdbdb'
-}
--- test/utils/generateRandomNum.js
+++
@@ -1,6 +0,0 @@
-let generateNum = function(){
-    const num = Math.random()*1000 // 0,1236 * 1000 = 123,6
-    return Math.ceil(num)
-}
-
-export { generateNum }
